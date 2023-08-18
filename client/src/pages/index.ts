1687b393cf68a76f6e5ff542ab011d7bfbc0a130
Vsevolod <mr.seva.021@gmail.com>
2023-08-18 21:29:21 +0300

    changed name of workspace page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,6 +1,6 @@
 export * from './RegistrationPage/RegistrationPage';
 export * from './NotFoundPage/NotFoundPage';
-export * from './ProfilePage/WorkspacePage';
+export * from './WorkspacePage/WorkspacePage';
 export * from './LoginPage/LoginPage';
 export * from './DesksPage/DesksPage';
 export * from './DeskPage/DeskPage';
